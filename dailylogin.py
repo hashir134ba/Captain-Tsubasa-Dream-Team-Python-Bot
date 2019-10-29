43aa771ba2cdeb2574616debe0538465a850371a
root <root@root>
2019-10-29 23:36:28 +0100

    fix dailylogin

--- dailylogin.py
+++ dailylogin.py
@@ -1,44 +1,39 @@
 # -*- coding: utf-8 -*-
-from multiprocessing import Pool
 from db import Database
 from main import API
 import base64
+import random
+import threading
+import Queue
 
 def login(i):
+	i=i.rstrip().strip()
 	id,pw=i.split(';')
 	try:
 		a=API()
 		#a.setproxy()
-		a.setUserId(id)
+		a.setUserId(int(id))
 		a.setPassword(base64.b64decode(pw))
 		a.dailylogin()
-		a.getAllMissions()
-		#if a.userinfo[3]['update_info']['user_billing_info']['free_stone']>=90:
-			#a.gacha_play(1042201,10)
-			#a.gacha_play(1042202,10)
-			#a.gacha_play(1042203,10)
-			#a.gacha_play(1042204,10)
-			#a.gacha_play(1042205,10)
-			#a.gacha_play(41,10)
-			#a.gacha_play(101,1)
-			#a.gacha_play(121,1)
-		a.user_fetchHomeInfo()
-		a.exportPlayers()
 	except:
 		return
 
+def wrapper_targetFunc(q):
+	while True:
+		try:
+			work = q.get()
+		except Queue.Empty:
+			return
+		login(work)
+		q.task_done()
+
 if __name__ == "__main__":
 	db=Database()
-	data=['%s;%s'%(x[0],x[1]) for x in db.getAllAccounts()]
+	data=set(['%s;%s'%(x[0],x[1]) for x in db.getAllAccounts()])
 	print len(data)
-	p= Pool(5)
-	try:
-		list(p.imap_unordered(login, data))
-	except Exception:
-		print("a worker failed, aborting...")
-		p.close()
-		p.terminate()
-	else:
-		p.close()
-		p.join()
-	print "job done"
+	q = Queue.Queue()
+	for i in data:
+		q.put_nowait(i)
+	for _ in range(400):
+		threading.Thread(target=wrapper_targetFunc,args=(q,)).start()
+	q.join()
--- db.py
+++ db.py
@@ -21,14 +21,22 @@ class Database(object):
 		c.execute("INSERT OR REPLACE INTO accounts (id,password,coin,free_stone,total_login_days) VALUES (%s,'%s',%s,%s,%s)"%(id,pw,coin,free_stone,total_login_days))
 		conn.commit()
 		conn.close()
-		
+
 	def updateAccount(self,id,coin,free_stone,total_login_days):
 		conn = sqlite3.connect(self.sqlite_file)
 		c = conn.cursor()
 		c.execute("UPDATE accounts SET coin=%s,free_stone=%s,total_login_days=%s where id=%s"%(coin,free_stone,total_login_days,id))
 		conn.commit()
 		conn.close()
-		
+
+	def rmAccount(self,id):
+		return
+		conn = sqlite3.connect(self.sqlite_file)
+		c = conn.cursor()
+		c.execute("delete from accounts where id=%s"%(id))
+		conn.commit()
+		conn.close()
+
 	def getAllAccounts(self,limit=None):
 		conn = sqlite3.connect(self.sqlite_file)
 		c = conn.cursor()
